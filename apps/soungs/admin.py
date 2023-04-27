727d2cd7d5d098cacaa3265192694156ebcbe4eb
Deissei <privetizkyrgyzstana2@gmail.com>
2023-04-27 23:33:55 +0600

    add logics in models

--- apps/soungs/admin.py
+++ apps/soungs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from apps.soungs.models import Soung, Album
+from apps.soungs.models import Soung, Album, Playlist
 
 
 admin.site.register(Soung)
 admin.site.register(Album)
+admin.site.register(Playlist)
--- apps/soungs/models.py
+++ apps/soungs/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 from apps.artists.models import Artist
 
 
 class Soung(models.Model):
+    listening = 0
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='artist_soung')
     title = models.CharField(max_length=100, unique=True)
     duration = models.IntegerField(default=1)
 
     soung_file = models.FileField(upload_to='soungs/')
+
+    
+    def point_listening(self):
+        self.listening =+ 1
+        return self.listening
 
     def __str__(self):
         return self.title
@@ -24,6 +32,15 @@ class Album(models.Model):
     facebook = models.CharField(max_length=200, null=True, blank=True)
     twitter = models.CharField(max_length=200, null=True, blank=True)
     vkontact = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_playlist')
+    soungs = models.ManyToManyField(Soung, related_name='soungs_playlist')
 
     def __str__(self):
         return self.title
